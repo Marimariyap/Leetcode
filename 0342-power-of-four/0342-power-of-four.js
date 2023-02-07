654a24fcd8a8b9bdc689de911359f156996baeb0
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-07 21:26:58 +0900

    Time: 86 ms (30.85%), Space: 43.5 MB (31.00%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    let x = 0;
+    while (4 ** x <= n) {
+        if(4 ** x === n){
+            return true
+        }else{
+            x++;
+        }
+    }
+    return false;
+
+
+};
