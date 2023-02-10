7a648c9bb0434b7674b2f3be30b06b82bb0796aa
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-10 12:13:10 +0900

    Time: 78 ms (70.45%), Space: 48.1 MB (65.67%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -4,12 +4,6 @@
  */
 var reverseWords = function(s) {
     
-    let arr = [];
-    
-    s = s.split(" ");
-    
-    for (var i = 0; i < s.length; i++) {
-        arr.push(s[i].split("").reverse().join(""));
-    }
-    return arr.join(" ")
+    let arr = s.split(" ");
+    return arr.map((i) => i.split("").reverse().join("")).join(" ");
 };
