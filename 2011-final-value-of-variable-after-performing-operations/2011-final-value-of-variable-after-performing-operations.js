dedd341b6db1a78d2cded9a681969c00fb4a64b4
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-13 12:38:06 +0900

    Time: 66 ms (77.95%), Space: 42.4 MB (63.66%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    return operations.reduce((acc,curr) => curr[1] === "+" ? ++acc : --acc, 0);
+    let x = 0;
+    operations.map((i) => {
+        if(i.includes("+")){
+            x += 1;
+        } else {
+            x -= 1;
+        }
+    });
+    return x;
 };
