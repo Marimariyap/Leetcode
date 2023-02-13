b8b04cda868d19367f9c05c20efc2dc4cf444189
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-13 13:36:23 +0900

    Time: 71 ms (38.13%), Space: 42.2 MB (30.94%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function(word, ch) {
+    if (word.includes(ch)) {
+        word = word.split("");
+
+        let arr = word.slice(0, word.indexOf(ch) + 1).reverse();
+        let arr_2 = word.slice(word.indexOf(ch) + 1, word.length);
+        return arr.concat(arr_2).join("");
+    } else {
+        return word;
+    }
+};
