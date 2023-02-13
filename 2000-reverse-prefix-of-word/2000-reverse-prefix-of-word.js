c6a7c9255830739568ceed2a9319a23f9c30923f
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-13 13:38:53 +0900

    Time: 60 ms (83.21%), Space: 42.3 MB (19.66%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
@@ -4,13 +4,13 @@
  * @return {string}
  */
 var reversePrefix = function(word, ch) {
-    if (word.includes(ch)) {
-        word = word.split("");
+        if (word.includes(ch)) {
+            word = word.split("");
 
-        let arr = word.slice(0, word.indexOf(ch) + 1).reverse();
-        let arr_2 = word.slice(word.indexOf(ch) + 1, word.length);
-        return arr.concat(arr_2).join("");
-    } else {
-        return word;
-    }
+            let arr = word.slice(0, word.indexOf(ch) + 1).reverse();
+            let arr_2 = word.slice(word.indexOf(ch) + 1, word.length);
+            return arr.concat(arr_2).join("");
+        } else {
+            return word;
+        }
 };
