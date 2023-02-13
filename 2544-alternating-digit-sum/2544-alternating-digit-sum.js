15456d8bea9f6bc74114ddbcdae1fedb0ed166c7
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-13 18:00:45 +0900

    Time: 54 ms (96.98%), Space: 42.3 MB (22.61%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var alternateDigitSum = function(n) {
+    n = n.toString().split("");
+
+    let sum = 0;
+    for (var i = 0; i < n.length; i++) {
+        if (i % 2 !== 0) {
+            sum += -n[i];
+        } else {
+            sum += +n[i];
+        }
+    }
+    return sum;
+};
