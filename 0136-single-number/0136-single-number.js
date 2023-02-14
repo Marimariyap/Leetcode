f9f1a12f5a32da9223a77cd8eebd04c5dd485408
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-02-14 15:38:21 +0900

    Time: 75 ms (71.62%), Space: 42.6 MB (96.58%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums = nums.sort((a,b) => a - b);
-
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[i - 1] && nums[i] !== nums[i + 1]) {
-            return nums[i];
-        }
-    }
+    return nums.reduce((acc,curr)=>acc^curr);
 };
