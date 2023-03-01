e5fd42408e6059fe4492b482145cff666ead8673
Marimariyap <112878606+Marimariyap@users.noreply.github.com>
2023-03-01 13:24:28 +0900

    Time: 120 ms (82.80%), Space: 46.7 MB (95.55%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -3,18 +3,13 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-    let arr = [];
-    let max = 0;
-
+    let sum = 0;
+    
     nums.sort((a, b) => a - b);
 
-    for (var i = 0; i < nums.length; i += 2) {
-        arr.push(nums.slice(i, i + 2));
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 === 0) sum += nums[i]
     }
 
-    for (var i = 0; i < arr.length; i++) {
-        max += Math.min(...arr.at(i));
-    }
-
-    return max;
+    return sum;
 };
